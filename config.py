d283db66292ae0a619b3cea93a9ce47481433c8c
pdaian <phil@linux.com>
2013-10-25 01:34:43 -0500

    change silly defaults in config

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ MAX_MESSAGES = 20000
 # Maximum number of days to keep a message in memory
 FORGET_AFTER_DAYS = 7
 # Port for Lethe HTTP server
-LETHE_PORT = 80
+LETHE_PORT = 5200
 # Allow connections outside of localhost
 # WARNING: This is dangerous and may expose your machine
 # It is recommended to run Lethe over anonymizing software
 # such as Tor or I2P behind a proxy.  Enable at your own risk!
-LISTEN_OUTSIDE_LOCALHOST = True
+LISTEN_OUTSIDE_LOCALHOST = False
